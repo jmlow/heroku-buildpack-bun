09069d2a142ba553aabf34b55cb52144c67650cf
Justin Ludlow <justinmludlow@gmail.com>
2025-03-31 00:39:42 -0700

    Update code for handling package managers and dependencies

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -8,37 +8,19 @@ list_dependencies() {
 	local build_dir="${1}"
 
 	cd "${build_dir}" || return
-	if ${YARN}; then
-		echo ""
-		(yarn list --depth=0 || true) 2>/dev/null
-		echo ""
-	else
-		(npm ls --depth=0 | tail -n +2 || true) 2>/dev/null
-	fi
+	(bun pm ls | tail -n +2 || true) 2>/dev/null
 }
 
 run_if_present() {
 	local build_dir="${1-}"
 	local script_name="${2-}"
 	local has_script_name
-	local script
 
 	has_script_name=$(has_script "${build_dir}/package.json" "${script_name}")
-	script=$(read_json "${build_dir}/package.json" ".scripts[\"${script_name}\"]")
 
 	if [[ ${has_script_name} == "true" ]]; then
-		if ${YARN} || ${YARN_2}; then
-			echo "Running ${script_name} (yarn)"
-			if [[ -n ${script} ]]; then
-				monitor "${script_name}-script" yarn run "${script_name}"
-			fi
-		elif ${PNPM}; then
-			echo "Running ${script_name}"
-			monitor "${script_name}-script" pnpm run --if-present "${script_name}"
-		else
-			echo "Running ${script_name}"
-			monitor "${script_name}-script" npm run "${script_name}" --if-present
-		fi
+		echo "Running ${script_name}"
+		monitor "${script_name}-script" bun run --bun --if-present "${script_name}"
 	fi
 }
 
@@ -52,37 +34,12 @@ run_build_if_present() {
 	script=$(read_json "${build_dir}/package.json" ".scripts[\"${script_name}\"]")
 
 	if [[ ${script} == "ng build" ]]; then
-		warn '"ng build" detected as build script. We recommend you use `ng build --prod` or add `--prod` to your build flags. See https://devcenter.heroku.com/articles/nodejs-support#build-flags'
+		warn '"ng build" detected as build script. We recommend you use `ng build --prod`.'
 	fi
 
 	if [[ ${has_script_name} == "true" ]]; then
-		if ${YARN} || ${YARN_2}; then
-			echo "Running ${script_name} (yarn)"
-			if [[ -n ${script} ]]; then
-				if [[ -n ${NODE_BUILD_FLAGS} ]]; then
-					echo "Running with ${NODE_BUILD_FLAGS} flags"
-					monitor "${script_name}-script" yarn run "${script_name}" "${NODE_BUILD_FLAGS}"
-				else
-					monitor "${script_name}-script" yarn run "${script_name}"
-				fi
-			fi
-		elif ${PNPM}; then
-			echo "Running ${script_name}"
-			if [[ -n ${NODE_BUILD_FLAGS} ]]; then
-				echo "Running with ${NODE_BUILD_FLAGS} flags"
-				monitor "${script_name}-script" pnpm run --if-present "${script_name}" -- "${NODE_BUILD_FLAGS}"
-			else
-				monitor "${script_name}-script" pnpm run --if-present "${script_name}"
-			fi
-		else
-			echo "Running ${script_name}"
-			if [[ -n ${NODE_BUILD_FLAGS} ]]; then
-				echo "Running with ${NODE_BUILD_FLAGS} flags"
-				monitor "${script_name}-script" npm run "${script_name}" --if-present -- "${NODE_BUILD_FLAGS}"
-			else
-				monitor "${script_name}-script" npm run "${script_name}" --if-present
-			fi
-		fi
+		echo "Running ${script_name}"
+		monitor "${script_name}-script" bun run --bun --if-present "${script_name}"
 	fi
 }
 
@@ -140,253 +97,12 @@ log_build_scripts() {
 	meta_set "heroku-postbuild-script" "$(read_json "${build_dir}/package.json" '.scripts["heroku-postbuild"]')"
 }
 
-yarn_node_modules() {
-	local build_dir="${1-}"
-	local production="${YARN_PRODUCTION:-false}"
-
-	echo "Installing node modules (yarn.lock)"
-	cd "${build_dir}" || return
-	monitor "yarn-install" yarn install --production="${production}" --frozen-lockfile --ignore-engines --prefer-offline 2>&1
-}
-
-yarn_2_install() {
-	local build_dir="${1-}"
-
-	echo "Running 'yarn install' with yarn.lock"
-	cd "${build_dir}" || return
-
-	monitor "yarn-2-install" yarn install --immutable 2>&1
-}
-
-yarn_prune_devdependencies() {
-	local build_dir="${1-}"
-	local cache_dir="${2-}"
-	local buildpack_dir="${3-}"
-
-	if [[ ${NODE_ENV} == "test" ]]; then
-		echo "Skipping because NODE_ENV is 'test'"
-		meta_set "skipped-prune" "true"
-		return 0
-	elif [[ ${NODE_ENV} != "production" ]]; then
-		echo "Skipping because NODE_ENV is not 'production'"
-		meta_set "skipped-prune" "true"
-		return 0
-	elif [[ -n ${YARN_PRODUCTION} ]]; then
-		echo "Skipping because YARN_PRODUCTION is '${YARN_PRODUCTION}'"
-		meta_set "skipped-prune" "true"
-		return 0
-	elif ${YARN_2}; then
-		if [[ ${YARN2_SKIP_PRUNING} == "true" ]]; then
-			echo "Skipping because YARN2_SKIP_PRUNING is '${YARN2_SKIP_PRUNING}'"
-			meta_set "skipped-prune" "true"
-			return 0
-		fi
-		cd "${build_dir}" || return
-		echo "Running 'yarn heroku prune'"
-		export YARN_PLUGINS="${buildpack_dir}/yarn2-plugins/prune-dev-dependencies/bundles/@yarnpkg/plugin-prune-dev-dependencies.js"
-		monitor "yarn-prune" yarn heroku prune
-		meta_set "skipped-prune" "false"
-	else
-		cd "${build_dir}" || return
-		monitor "yarn-prune" yarn install --frozen-lockfile --ignore-engines --ignore-scripts --prefer-offline 2>&1
-		meta_set "skipped-prune" "false"
-	fi
-}
-
-has_npm_lock() {
-	local build_dir="${1-}"
-
-	if [[ -f "${build_dir}/package-lock.json" ]] || [[ -f "${build_dir}/npm-shrinkwrap.json" ]]; then
-		echo "true"
-	else
-		echo "false"
-	fi
-}
-
-should_use_npm_ci() {
-	local build_dir="${1-}"
-	local npm_version
-	local major
-
-	npm_version=$(npm --version)
-	major=$(npm_version_major)
-
-	if [[ -f "${build_dir}/package.json" ]] && [[ "$(has_npm_lock "${build_dir}")" == "true" ]] && ((major >= 6)); then
-		echo "true"
-	else
-		echo "false"
-	fi
-}
-
-npm_node_modules() {
-	local build_dir="${1-}"
-	local production="${NPM_CONFIG_PRODUCTION:-false}"
-
-	if [[ -e "${build_dir}/package.json" ]]; then
-		cd "${build_dir}" || return
-
-		if [[ ${USE_NPM_INSTALL} == "false" ]]; then
-			meta_set "use-npm-ci" "true"
-			echo "Installing node modules"
-			monitor "npm-install" npm ci --production="${production}" --unsafe-perm --userconfig "${build_dir}/.npmrc" 2>&1
-		else
-			meta_set "use-npm-ci" "false"
-			if [[ -e "${build_dir}/package-lock.json" ]]; then
-				echo "Installing node modules (package.json + package-lock)"
-			elif [[ -e "${build_dir}/npm-shrinkwrap.json" ]]; then
-				echo "Installing node modules (package.json + shrinkwrap)"
-			else
-				echo "Installing node modules (package.json)"
-			fi
-			monitor "npm-install" npm install --production="${production}" --unsafe-perm --userconfig "${build_dir}/.npmrc" 2>&1
-		fi
-	else
-		echo "Skipping (no package.json)"
-	fi
-}
-
-npm_rebuild() {
-	local build_dir="${1-}"
-	local production="${NPM_CONFIG_PRODUCTION:-false}"
-
-	if [[ -e "${build_dir}/package.json" ]]; then
-		cd "${build_dir}" || return
-		echo "Rebuilding any native modules"
-		npm rebuild 2>&1
-		if [[ -e "${build_dir}/npm-shrinkwrap.json" ]]; then
-			echo "Installing any new modules (package.json + shrinkwrap)"
-		else
-			echo "Installing any new modules (package.json)"
-		fi
-		monitor "npm-rebuild" npm install --production="${production}" --unsafe-perm --userconfig "${build_dir}/.npmrc" 2>&1
-	else
-		echo "Skipping (no package.json)"
-	fi
-}
-
-npm_prune_devdependencies() {
-	local npm_version
-	local build_dir="${1-}"
-
-	npm_version=$(npm --version)
-
-	if [[ ${NODE_ENV} == "test" ]]; then
-		echo "Skipping because NODE_ENV is 'test'"
-		meta_set "skipped-prune" "true"
-		return 0
-	elif [[ ${NODE_ENV} != "production" ]]; then
-		echo "Skipping because NODE_ENV is not 'production'"
-		meta_set "skipped-prune" "true"
-		return 0
-	elif [[ -n ${NPM_CONFIG_PRODUCTION} ]]; then
-		echo "Skipping because NPM_CONFIG_PRODUCTION is '${NPM_CONFIG_PRODUCTION}'"
-		meta_set "skipped-prune" "true"
-		return 0
-	elif [[ ${npm_version} == "5.3.0" ]]; then
-		mcount "skip-prune-issue-npm-5.3.0"
-		echo "Skipping because npm 5.3.0 fails when running 'npm prune' due to a known issue"
-		echo "https://github.com/npm/npm/issues/17781"
-		echo ""
-		echo "You can silence this warning by updating to at least npm 5.7.1 in your package.json"
-		echo "https://devcenter.heroku.com/articles/nodejs-support#specifying-an-npm-version"
-		meta_set "skipped-prune" "true"
-		return 0
-	elif [[ ${npm_version} == "5.6.0" ]] ||
-		[[ ${npm_version} == "5.5.1" ]] ||
-		[[ ${npm_version} == "5.5.0" ]] ||
-		[[ ${npm_version} == "5.4.2" ]] ||
-		[[ ${npm_version} == "5.4.1" ]] ||
-		[[ ${npm_version} == "5.2.0" ]] ||
-		[[ ${npm_version} == "5.1.0" ]]; then
-		mcount "skip-prune-issue-npm-5.6.0"
-		echo "Skipping because npm ${npm_version} sometimes fails when running 'npm prune' due to a known issue"
-		echo "https://github.com/npm/npm/issues/19356"
-		echo ""
-		echo "You can silence this warning by updating to at least npm 5.7.1 in your package.json"
-		echo "https://devcenter.heroku.com/articles/nodejs-support#specifying-an-npm-version"
-		meta_set "skipped-prune" "true"
-		return 0
-	else
-		cd "${build_dir}" || return
-		monitor "npm-prune" npm prune --userconfig "${build_dir}/.npmrc" 2>&1
-		meta_set "skipped-prune" "false"
-	fi
-}
-
-pnpm_install() {
-	local build_dir="${1-}"
-	local cache_dir="${2-}"
-
-	echo "Running 'pnpm install' with pnpm-lock.yaml"
-	cd "${build_dir}" || return
-
-	monitor "pnpm-install" pnpm install --prod=false --frozen-lockfile 2>&1
-
-	counter=$(load_pnpm_prune_store_counter "${cache_dir}")
-	if ((counter == 0)); then
-		echo "Cleaning up pnpm store"
-		suppress_output pnpm store prune
-	fi
-	save_pnpm_prune_store_counter "${cache_dir}" "$((counter - 1))"
-}
-
-pnpm_prune_devdependencies() {
-	local build_dir="${1-}"
-
-	cd "${build_dir}" || return
-
-	if [[ ${NODE_ENV} == "test" ]]; then
-		echo "Skipping because NODE_ENV is 'test'"
-		meta_set "skipped-prune" "true"
-		return 0
-	elif [[ ${NODE_ENV} != "production" ]]; then
-		echo "Skipping because NODE_ENV is not 'production'"
-		meta_set "skipped-prune" "true"
-		return 0
-	elif [[ ${PNPM_SKIP_PRUNING} == "true" ]]; then
-		echo "Skipping because PNPM_SKIP_PRUNING is '${PNPM_SKIP_PRUNING}'"
-		meta_set "skipped-prune" "true"
-		return 0
-	elif [[ -f "${build_dir}/pnpm-workspace.yaml" ]] || [[ -f "${build_dir}/pnpm-workspace.yml" ]]; then
-		echo "Skipping because pruning is not supported for pnpm workspaces (https://pnpm.io/cli/prune)"
-		meta_set "skipped-prune" "true"
-		return 0
-	fi
-
-	pnpm_version=$(pnpm --version)
-	pnpm_major_version=$(echo "${pnpm_version}" | cut -d "." -f 1)
-	pnpm_minor_version=$(echo "${pnpm_version}" | cut -d "." -f 2)
-	pnpm_patch_version=$(echo "${pnpm_version}" | cut -d "." -f 3)
-
-	pnpm_prune_args=("prune" "--prod")
-
-	if (("${pnpm_major_version}" < 8)) ||
-		(("${pnpm_major_version}" == 8 && "${pnpm_minor_version}" < 15)) ||
-		(("${pnpm_major_version}" == 8 && "${pnpm_minor_version}" == 15 && "${pnpm_patch_version}" < 6)); then
-		if [[ -n "$(read_json "${build_dir}/package.json" '.scripts."pnpm:devPreinstall"')" ]] ||
-			[[ -n "$(read_json "${build_dir}/package.json" ".scripts.preinstall")" ]] ||
-			[[ -n "$(read_json "${build_dir}/package.json" ".scripts.install")" ]] ||
-			[[ -n "$(read_json "${build_dir}/package.json" ".scripts.postinstall")" ]] ||
-			[[ -n "$(read_json "${build_dir}/package.json" ".scripts.prepare")" ]]; then
-			warn_skipping_unsafe_pnpm_prune "${pnpm_version}"
-			meta_set "skipped-prune" "true"
-			return
-		fi
-	else
-		pnpm_prune_args+=("--ignore-scripts")
-	fi
-
-	pnpm "${pnpm_prune_args[@]}" 2>&1
-
-	meta_set "skipped-prune" "false"
-}
-
 bun_install() {
 	local build_dir="${1-}"
 
 	cd "${build_dir}" || return
 	echo "Running 'bun install'"
-	monitor "bun-install" bun install 2>&1
+	monitor "bun-install" bun install --frozen-lockfile 2>&1
 }
 
 bun_prune_devdependencies() {
@@ -394,11 +110,7 @@ bun_prune_devdependencies() {
 
 	cd "${build_dir}" || return
 
-	if [[ ${BUN_ENV} == "test" ]]; then
-		echo "Skipping because BUN_ENV is 'test'"
-		meta_set "skipped-prune" "true"
-		return 0
-	elif [[ ${BUN_ENV} != "production" ]]; then
+	if [[ ${BUN_ENV} != "production" ]]; then
 		echo "Skipping because BUN_ENV is not 'production'"
 		meta_set "skipped-prune" "true"
 		return 0
@@ -408,6 +120,6 @@ bun_prune_devdependencies() {
 		return 0
 	fi
 
-	monitor "bun-prune" bun install --production 2>&1
+	monitor "bun-prune" bun install -p --frozen-lockfile --ignore-scripts 2>&1
 	meta_set "skipped-prune" "false"
 }
